7398678777fa6c81ead1bb973bbd0336d90cbab8
yuyi5187 <kimyy200063@gmail.com>
2023-07-09 20:24:16 +0900

    interface 연습코드

---
+++ src/dev/aquashdw/CarInterface.java
@@ -0,0 +1,7 @@
+package dev.aquashdw;
+
+public interface CarInterface {
+    void setDriver(Driver driver);
+    void accelerate();
+    void brake();
+}
---
+++ src/dev/aquashdw/Road.java
@@ -0,0 +1,14 @@
+package dev.aquashdw;
+
+import java.util.ArrayList;
+
+public class Road {
+    private ArrayList<CarInterface> carsOnRoad;
+
+    public Road(){
+    }
+
+    public void addCar(CarInterface car){
+        this.carsOnRoad.add(car);
+    }
+}
