90f9837e884b01ab46ffe0db5be9a2e9a3714b00
yuyi5187 <kimyy200063@gmail.com>
2023-07-09 20:41:59 +0900

    interface 연습코드2

--- src/dev/aquashdw/Main.java
+++ src/dev/aquashdw/Main.java
@@ -1,15 +1,13 @@
 package dev.aquashdw;
 
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Driver unlicensed = new Driver("unlicensed", 0);
-        Driver me = new Driver("me", 1);
-        CarInterface car = new GoCart();
-        car.setDriver(unlicensed);
-        car.setDriver(me);
+        List<CarInterface> linkedList = new LinkedList<>();
+        Road route88 = new Road(linkedList);
 
-        car = new SmallCar();
-        car.setDriver(me);
-        car.setDriver(unlicensed);
     }
 }
--- src/dev/aquashdw/Road.java
+++ src/dev/aquashdw/Road.java
@@ -1,13 +1,18 @@
 package dev.aquashdw;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Road {
     private ArrayList<CarInterface> carsOnRoad;
 
     public Road(){
+        this.carsOnRoad = new ArrayList<>();
     }
-
+    public Road(List<CarInterface> carsOnRoad){
+        this.carsOnRoad = (ArrayList<CarInterface>) carsOnRoad;
+    }
     public void addCar(CarInterface car){
         this.carsOnRoad.add(car);
     }
